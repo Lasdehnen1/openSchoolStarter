eb621cd31cc3aed15c4ed4beed0d2f2a446655e6
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-20 16:23:20 +0300

    Initial commit

---
+++ src/main/java/com/example/openschoolstarter/OpenSchoolStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.openschoolstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSchoolStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSchoolStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/openschoolstarter/OpenSchoolStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.openschoolstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSchoolStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
