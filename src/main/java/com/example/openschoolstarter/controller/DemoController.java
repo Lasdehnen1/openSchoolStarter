f4bfbaa727a0401074175bc1b9c82e223a4b28e2
Evgenii.Pichugov <eugepic@yandex.ru>
2024-06-26 15:39:37 +0300

    more docs added

--- src/main/java/com/example/openschoolstarter/controller/DemoController.java
+++ src/main/java/com/example/openschoolstarter/controller/DemoController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * This controller just for showing how logging works.
+ * This controller just for showing how logging actually works.
  *
  */
 @RestController
